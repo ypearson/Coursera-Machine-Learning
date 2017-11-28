9fe4a360048ac7189c3d10b2839f9fbec689d6af
Yvan Pearson <ypearson@nklabs.com>
2017-11-27 22:00:33 -0500

    finished last assigment

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -1,5 +1,4 @@
-function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
-                                  num_features, lambda)
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda)
 %COFICOSTFUNC Collaborative filtering cost function
 %   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
 %   num_features, lambda) returns the cost and gradient for the
@@ -7,11 +6,10 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 %
 
 % Unfold the U and W matrices from params
-X = reshape(params(1:num_movies*num_features), num_movies, num_features);
-Theta = reshape(params(num_movies*num_features+1:end), ...
-                num_users, num_features);
+X     = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +19,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,31 +27,46 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+ J = (1/2) * sum( sum(  ( (X * Theta' - Y) .* R) .^2 ) ) + (lambda/2)*(  sum(sum(Theta.^2)) + sum(sum(X.^2))  );
 
+% for j=1:num_movies
+%         for k=1:num_features
+%             for i=1:num_users
+%                     Theta_grad(i,k) = ( ( X(j,:) * Theta(i,:)' - Y(j,i) ) * X(j,k) * R(j,i) ) + Theta_grad(i,k);
+%         end
+%     end
+% end
 
+% for j=1:num_movies
+%         for k=1:num_features
+%             for i=1:num_users
+%                     X_grad(j,k) = ( ( X(j,:) * Theta(i,:)' - Y(j,i) ) .* Theta(i,k) * R(j,i) ) + X_grad(j,k);
+%         end
+%     end
+% end
 
+for i=1:num_users
 
+    Theta_grad(i,:) = ( ( (X * Theta')(:,i) - Y(:,i)) .* R(:,i) )' * X + lambda*Theta(i,:);
 
+end
 
+for j=1:num_movies
 
+    X_grad(j,:) = ( ( (X * Theta')(j,:) - Y(j,:)) .* R(j,:) ) * Theta  + lambda*X(j,:);
 
-
-
-
-
-
-
+end
 
 % =============================================================
 
--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -12,30 +12,31 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
     %               end of the loop will compare the F1 score for this
     %               choice of epsilon and set it to be the best epsilon if
     %               it is better than the current choice of epsilon.
-    %               
+    %
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    predictions = (pval < epsilon);
 
+    true_pos     = sum( predictions &  yval);
+    true_neg     = sum(~predictions & ~yval);
 
+    false_pos     = sum( predictions & ~yval);
+    false_neg     = sum(~predictions &  yval);
 
+    precision = true_pos / ( true_pos + false_pos );
+    recall    = true_pos / ( true_pos + false_neg );
 
+    F1 = 2 * (precision*recall) / (precision + recall);
 
-
-
-
-
-
-
-
-    % =============================================================
+% =============================================================
 
     if F1 > bestF1
        bestF1 = F1;
